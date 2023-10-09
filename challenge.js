45d16b7374054443507e3d2fb76324a535c29a9c
Mohamed <mohamed@Mohameds-Air.cust.communityfibre.co.uk>
2023-10-09 19:29:04 +0100

    added some comments

--- challenge.js
+++ challenge.js
@@ -1,36 +1,39 @@
-for(let i = 0; i < 20; i++) {
-    console.log("Ahmed")
-}
+
+// // write a for loop that prints your name 20 times.
+
+// for(let i = 0; i < 20; i++) {
+//     console.log("Ahmed")
+// }
+
+// let i = 0;
+// while (i < 20) {
+//     console.log("Dini");
+//     i++;
+// }
 
 
+// // write if/else if, chained code to check what day of the it is
 
-let i = 0;
-while (i < 20) {
-    console.log("Dini");
-    i++;
-}
+// let today = "monday";
 
+// if(today == "Monday") {
+//     console.log("It is Coding Day")
+// } else if (today == "Tuesday") {
+//     console.log("It is my off day")
+// } else {
+//     console.log("ask me personally which day")
+// }
 
-let today = "monday";
+// // convert the above code to switch from if/else if
+// switch (today) {
+//     case "Monday":
+//         console.log("It is Coding Day, from switch")
+//         break;
+//     case "Tuesday":
+//         console.log("It is my off day, from switch")
+//         break;
 
-if(today == "Monday") {
-    console.log("It is Coding Day")
-} else if (today == "Tuesday") {
-    console.log("It is my off day")
-} else {
-    console.log("ask me personally which day")
-}
-
-
-switch (today) {
-    case "Monday":
-        console.log("It is Coding Day, from switch")
-        break;
-    case "Tuesday":
-        console.log("It is my off day, from switch")
-        break;
-
-    default:
-        console.log("ask me personally which day, from switch")
-        break;
-}
+//     default:
+//         console.log("ask me personally which day, from switch")
+//         break;
+// }
